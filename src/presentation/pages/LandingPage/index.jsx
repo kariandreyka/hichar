6574c465996248f093cb042f7d4ce38a3ad21979
halparn <39450799+halparn@users.noreply.github.com>
2021-06-24 11:07:54 +0300

    Update index.jsx

--- src/presentation/pages/LandingPage/index.jsx
+++ src/presentation/pages/LandingPage/index.jsx
@@ -5,10 +5,21 @@ import WrapperContent from './Wrapper/Wrapper';
 
 const LandingPage = () => {
     const [state, setstate] = useState(0);
+
     const handleMyBranch = () => {
         console.log('this is for branch testing');
     }
 
+   const hadnleSomeShit = (params) => {
+    console.log('this is for branch testinggg');
+        
+    }
+
+    const handleFalanFilan = () => {
+        console.log('fill');
+    }
+    
+  
     return (
         <LandingPageStyles>
             <WrapperContent />
